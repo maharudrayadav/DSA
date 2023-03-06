ce73b17ac8fc9ac53e8ccc24f197d07c2b144405
yadav <mby212001@gmail.com>
2023-03-06 12:08:33 +0530

    added some code

--- src/Strings/PrintStringInSubstringFormat.java
+++ src/Strings/PrintStringInSubstringFormat.java
@@ -5,8 +5,9 @@ public class PrintStringInSubstringFormat {
         String str="abcd";
         for(int i=0;i<=3;i++){
             for(int j=i+1;j<=4;j++){
-                System.out.println(str.substring(i,j));
+                System.out.print(str.substring(i,j));
             }
+            System.out.println(" ");
         }
     }
 }
